f630df09ebe737a10665d19205296c9101399558
nishamvp <107843151+nishamvp@users.noreply.github.com>
2024-08-13 07:20:55 +0400

    Controller and router created,authentication api completed,jwt signed

---
+++ server/controllers/authentication.js
@@ -0,0 +1,111 @@
+import prisma from "../db/prisma.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../utils/jwt.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    // Validate input
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        status: "failed",
+        message: "Please provide username, email, and password",
+      });
+    }
+
+    // Check if the user already exists
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(409).json({
+        status: "failed",
+        message: "Email already exists, please try logging in",
+      });
+    }
+
+    // Check for unique username
+    const existingUsername = await prisma.user.findUnique({
+      where: { username },
+    });
+    if (existingUsername) {
+      return res
+        .status(409)
+        .json({ status: "failed", message: "Username is taken" });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return res
+      .status(201)
+      .json({ status: "success", message: "User created successfully" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Validate input
+    if (!email || !password) {
+      return res.status(400).json({
+        status: "failed",
+        message: "Please provide email and password",
+      });
+    }
+
+    // Check if the user exists
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (!existingUser) {
+      return res.status(404).json({
+        status: "failed",
+        message: "User not found, please register first",
+      });
+    }
+
+    // Check if the password is correct
+    const isValidPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isValidPassword) {
+      return res
+        .status(401)
+        .json({ status: "failed", message: "Invalid username or password" });
+    }
+
+    // Generate JWT token
+    const token = await generateToken({ email: existingUser.email });
+
+    // Set token as cookie
+    const options = {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+      sameSite: "none",
+      secure: false,
+    };
+    res.cookie("token", token, options);
+
+    return res
+      .status(200)
+      .json({ status: "success", message: "Login successful" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal Server Error" });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import prisma from "./db/prisma.js";
+import router from "./routers/user.js";
 
 dotenv.config(); // Load environment variables
 
@@ -9,21 +10,7 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    try {
-        await prisma.user.create({
-            data: {
-                username: "nisham", 
-                email: "sam@gmail.com",
-                password: "password"
-            }
-        });
-        res.status(200).send("User created successfully.");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
+app.use('/user',router)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
